04562ac7741cb85b22e42fd3827f8f29a8880e01
saplealdrin <saplealdrin@asiadt.biz>
2026-05-28 13:23:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function contactSupport() {
+  alert("This is a demo button. You can connect this to Intercom, HubSpot, or your own contact page.");
+
+  // Example for Intercom:
+  // if (window.Intercom) {
+  //   window.Intercom('showNewMessage');
+  // }
+}
